c9f5a743de1dcd6ece911f894d65811253705918
nitishmdb <nitishmdb@gmail.com>
2021-02-26 18:51:50 +0530

    Initital commit

---
+++ Extension/IdentityExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace WebApplicationMVC5.Extension
+{
+    public static class IdentityExtensions
+    {
+        public static string GetRoleName(this IIdentity identity)
+        {
+            if (identity == null)
+            {
+                throw new ArgumentNullException("identity");
+            }
+            var ci = identity as ClaimsIdentity;
+            if (ci != null)
+            {
+                return ci.FindFirstValue(ClaimTypes.Role);
+            }
+            return null;
+        }
+    }
+}
